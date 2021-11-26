eda71a1956d1dee1f2ce09b697e3613e2f5f35a0
shynu.v <80030442+shynu-v@users.noreply.github.com>
2021-11-26 19:11:31 +0530

    second

--- list.js
+++ list.js
@@ -1,52 +1,41 @@
+window.addEventListener("load", load);
+var disabled = 0;
+function load() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      response = JSON.parse(this.responseText);
 
-window.addEventListener("load",load);
-
-function load(){
-            var xhttp = new XMLHttpRequest ();
-        xhttp.onreadystatechange = function(){
-            if(this.readyState==4&&this.status==200){
-             response =JSON.parse(this.responseText);
-        
-             var list="";
-             for (let i = 0; i <response.length; i++) {
-                 if(response[i].completed===true){
-                 list += `<li><input  type="checkbox" checked disabled><label  class="strikethrough" >${response[i].title}</label></li>`;
-                 disabled += 1 
-                     
-             }else{
-                 list += `<li><input  type="checkbox" onchange="check()"><label  class="strikethrough" >${response[i].title}</label></li>`;
-             }
-          }
-          document.getElementById("list").innerHTML= list;
-              }
+      var list = "";
+      for (let i = 0; i < response.length; i++) {
+        if (response[i].completed === true) {
+          list += `<li class="list-group-item d-flex justify-content-between align-items-center"><input  type="checkbox" checked disabled><label  class="strikethrough" >${response[i].title}</label></li>`;
+          disabled += 1;
+        } else {
+          list += `<li class="list-group-item d-flex justify-content-between align-items-center"><input  type="checkbox" onchange="check()"><label  class="strikethrough" >${response[i].title}</label></li>`;
         }
-        xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
-        xhttp.send();
+      }
+      document.getElementById("list").innerHTML = list;
+    }
+  };
+  xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+  xhttp.send();
 }
-var disabled=0
+var checked;
+function check() {
+  let promise = new Promise(function (resolve, reject) {
+    let checkboxes = document.querySelectorAll(
+      'input[type="checkbox"]:checked '
+    );
 
-
-
-
-function check(){
-   
-
-        var promise = new Promise(function(resolve,reject){
-        
-    
-       
-         var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked ');
-
-       
-        var checked = checkboxes.length-disabled;
-        if(checked==5){
-            resolve();
-        }   
-    
-    });
-    promise.then(function(){
-        setTimeout(function(){
-            alert("Five Tasks have been Successfully Completed ");
-        },100)
-    })
+    checked = checkboxes.length - disabled;
+    if (checked % 5 == 0 && checked !=0) {
+      resolve(checked);
+    }
+  });
+  promise.then(function () {
+    setTimeout(function (a = checked) {
+      alert(a+" Tasks have been Successfully Completed ");
+    }, 100);
+  });
 }
